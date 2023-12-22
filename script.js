83ec90ba8deb17eb71ba5500391f346cb199d6c5
karolinakukielka <154562848+karolinakukielka@users.noreply.github.com>
2023-12-22 01:09:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function feliz() {
+    song = document.getElementById("song");
+    song.src = "feliz.mp3";
+    song.play();
+}
+
+function alli() {
+    song = document.getElementById("song");
+    song.src = "all.mp3";
+    song.play();
+}
+
+function last() {
+    song = document.getElementById("song");
+    song.src = "last.mp3";
+    song.play();
+}
+
